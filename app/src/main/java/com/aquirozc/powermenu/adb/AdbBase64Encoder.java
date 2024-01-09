9ad63cf27103ef7adfdd5d35c9784f9f1b617a62
aquirozc <alejandroqc03@outlook.com>
2024-01-09 09:22:10 -0600

    Lowered min api to level 24

--- app/src/main/java/com/aquirozc/powermenu/adb/AdbBase64Encoder.java
+++ app/src/main/java/com/aquirozc/powermenu/adb/AdbBase64Encoder.java
@@ -1,13 +1,13 @@
 package com.aquirozc.powermenu.adb;
 
 import com.tananaev.adblib.AdbBase64;
-import java.util.Base64;
+import android.util.Base64;
 
 public class AdbBase64Encoder implements AdbBase64 {
 
     @Override
     public String encodeToString(byte[] data) {
-        return Base64.getEncoder().encodeToString(data);
+        return Base64.encodeToString(data,Base64.NO_WRAP);
     }
 
 }
